d2a30860abeb7a1b95afa975f2bdf73dfb8fb459
Boss Panutat <panutatr@hotmail.com>
2021-02-03 19:43:55 +0700

    add sth

---
+++ num1.py
@@ -0,0 +1,24 @@
+import hashlib
+import os
+from itertools import product
+
+# r = "hello"
+enc_pas = "d54cc1fe76f5186380a0939d2fc1723c44e8a5f7"
+
+def hsha1(t):   #hash in sha1 format
+    return hashlib.sha1(t.encode()).hexdigest()
+
+# def hmd5(t):   #hash in sha1 format
+#     return hashlib.md5(t.encode()).hexdigest()
+
+fr = open('text.txt', 'r')
+Lines = fr.readlines()
+
+for line in Lines:
+    w = map(''.join, product(*((c.upper(),c.lower()) for c in line)))
+    for i in w:
+        # print(i.strip())
+        if hsha1(i.strip()) == enc_pas:
+            print(i.strip())
+            exit()
+print("nope")
---
+++ num2.py
@@ -0,0 +1,60 @@
+import os
+import time
+import hashlib
+from itertools import product
+
+def hsha1(t):   #hash in sha1 format
+    return hashlib.sha1(t.encode("utf-8")).hexdigest()
+
+# def hmd5(t):   #hash in sha1 format
+#     return hashlib.md5(t.encode()).hexdigest()
+
+#return list of mix upper and lower case characters.
+def ULmix(w):
+    return list(set(map(''.join, product(*((c.upper(),c.lower()) for c in w)))))
+
+def filler(word):
+    combos = []
+    for c in word:
+        if c not in options:
+            combos.append((c,))
+        else:
+            ex = [c]
+            ex.extend(options[c])
+            combos.append(ex)
+    return list(set((''.join(o) for o in product(*combos))))
+
+options = {
+  'o': ['0'],
+  'l': ['1'],
+  'i': ['1']}
+
+start = time.time()
+
+fr = open('text.txt', 'r')
+fw = open('dictionary.txt', 'w')
+Lines = fr.readlines()
+
+#store all data
+password_list = [] #password combination list
+# hash_list = [] #hash
+
+
+#read word from text list
+for l in Lines:                             # password
+    w_list = ULmix(l.strip())               # w_list = ['PASSWORD', 'PASWORd', ....]
+    # w.extend(word_mix)
+    for w in w_list:                        #!!!the original value is also gone!!!
+        f_list = filler(w)                  # f_list = [...,'PASSw0rD',...]
+        for f in f_list:
+            password_list.append((f.strip(),hsha1(f.strip())))
+
+for i in password_list:
+    fw.write(i[0] + "   " + i[1] + '\n')
+
+end = time.time()
+ 
+print('Time: ' + str(end - start) + '\n')
+fr.close()
+fw.close()
+print("Done")
